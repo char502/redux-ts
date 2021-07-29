847734ee14004f76450a0e02e8cdf4bfdc2ec8a1
charlotte@virtualtrips.io <charlotte@virtualtrips.io>
2021-07-29 15:34:51 +0100

    set up redux actions and reorganised definitions into seperate files

---
+++ src/state/action-types/index.ts
@@ -0,0 +1,6 @@
+// An enum is an object which sets up a variety of different properties that all have a closely related definition
+export enum ActionType {
+  SEARCH_REPOSITORIES = 'search_repositories',
+  SEARCH_REPOSITORIES_SUCCESS = 'search_repositories_success',
+  SEARCH_REPOSITORIES_ERROR = 'search_repositories_error'
+}
--- src/state/reducers/repositoriesReducer.ts
+++ src/state/reducers/repositoriesReducer.ts
@@ -1,3 +1,6 @@
+import { ActionType } from '../action-types';
+import { Action } from '../actions';
+
 interface RepositoriesState {
   loading: boolean;
   error: string | null;
@@ -5,6 +8,30 @@ interface RepositoriesState {
 }
 
 
-const reducer = (state, action) => {};
+const reducer = (
+  state: RepositoriesState,
+  action: Action
+): RepositoriesState => {
+  // if (action.type === 'search_repositiories_success') {
+  //   // 100% certainty that 'action' satisfies the
+  //   // SearchRepositoriesSuccessAction interface
+  //   action.payload
+  // }
+
+  switch (action.type) {
+    case ActionType.SEARCH_REPOSITORIES:
+      return { loading: true, error: null, data: [] };
+
+    case ActionType.SEARCH_REPOSITORIES_SUCCESS:
+      // 100% certain that 'action' is SearchRepositoriesSuccessAction
+      return { loading: false, error: null, data: action.payload };
+
+    case ActionType.SEARCH_REPOSITORIES_ERROR:
+      return { loading: false, error: action.payload, data: [] };
+
+    default:
+      return state;
+  }
+};
 
 export default reducer;
