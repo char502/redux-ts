8cd51850ec40187d2531394eadd7c84de2bf90ca
charlotte@virtualtrips.io <charlotte@virtualtrips.io>
2021-07-30 09:48:30 +0100

    finished redux-typescript example

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,15 +1,22 @@
 import { useState } from 'react';
-import { useActions } from '../hooks/useActions'
+// useSelector similar to mapStateToProps
+import { useTypedSelector } from '../hooks/useTypedSelector';
+import { useActions } from '../hooks/useActions';
 
 const RepositoriesList: React.FC = () => {
   const [term, setTerm] = useState('');
 
-  // This version of searchRepositories is now already bound to the dispatch function 
+  // This version of searchRepositories is now already bound to the dispatch function
   // because of bindActionCreators in the useActions Hook
   // As soon as this is called, it is equivalent to the previous dispatch call:
   // dispatch(actionCreators.searchRepositories(term));
   const { searchRepositories } = useActions();
-  
+
+  // react redux does not know about the type of data inside our redux store
+  const { data, error, loading } = useTypedSelector(
+    (state) => state.repositories
+  );
+  console.log(data, error, loading);
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -27,6 +34,10 @@ const RepositoriesList: React.FC = () => {
         />
         <button>Search</button>
       </form>
+
+      {error && <h3>{error}</h3>}
+      {loading && <h3>Loading...</h3>}
+      {!error && !loading && data.map((name) => <div key={name}>{name}</div>)}
     </div>
   );
 };
--- src/state/reducers/index.ts
+++ src/state/reducers/index.ts
@@ -6,3 +6,9 @@ const reducers = combineReducers({
 });
 
 export default reducers;
+
+// This makes use of a built in helper inside of typescript to say
+// take the 'reducer' function and give us back the type of whatever
+// that function returns
+// then assign this to 'RootState'
+export type RootState = ReturnType<typeof reducers>;
